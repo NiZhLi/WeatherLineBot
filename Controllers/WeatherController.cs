e6736f9dd89e37b499915648d019e47c0ca78a9e
linia <linia@LAPTOP-O2H02S37>
2026-01-02 18:28:45 +0800

    加入專案檔案。

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherBot.Services;
+using WeatherBot.Dtos;
+
+namespace WeatherBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController(WeatherOpenDataService weatherService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WeatherResponseDto>>> GetWeather([FromQuery] string city)
+        {
+          
+            var weatherData = await weatherService.GetWeatherForecast36HrByTwLocationAsync(city);
+            if (weatherData == null)
+            {
+                return BadRequest();
+            }
+            return Ok(weatherData);
+        }
+    }
+}
---
+++ Services/WeatherOpenDataService.cs
@@ -0,0 +1,26 @@
+﻿using WeatherBot.Dtos;
+
+namespace WeatherBot.Services
+{
+    public class WeatherOpenDataService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        private readonly string apiKey = configuration["ApiKey"];
+        private const string baseUrl = $"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001";
+        public async Task<WeatherResponseDto> GetWeatherForecast36HrByTwLocationAsync(string locationZhTw)
+        {
+            var client = httpClientFactory.CreateClient();
+            var RequestURL = $"{baseUrl}?Authorization={apiKey}&locationName={locationZhTw}";
+            var response = await client.GetAsync(RequestURL);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<WeatherResponseDto>();
+                return result;
+            }
+            
+            throw new Exception($"Status Code：{response.StatusCode}, Message：{await response.Content.ReadAsStringAsync()}");
+        }
+
+
+    }
+}
